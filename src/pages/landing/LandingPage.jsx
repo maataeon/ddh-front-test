4a7b759a295e2a73360d020385f2dc262772cc87
Matias Dominguez <ex5290@carrerasgl.sa>
2024-06-29 22:40:15 -0300

    add landing page

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -1,32 +1,40 @@
-import './landingPage.css'
-import backgroundLanding from "../../assets/background-landing.jpg";
-import { Button, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
+import "./landingPage.css";
+import backgroundLanding from "../../assets/landing.svg";
+import { Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 import WhatsAppIcon from "../../assets/whatsapp-icon.svg";
 
 const LandingPage = () => {
   return (
     <div className="Page">
-      <div className="LandingPage-Background" style={{backgroundImage: `url(${backgroundLanding})`}}></div>
+      {/*<div
+        className="LandingPage-Background"
+        style={{ backgroundImage: `url(${backgroundLanding})` }}
+      ></div>
       <div className="Landing-Portada">
-        <Typography className="Portada-Titulo">Reciclaje de componentes electrónicos</Typography>
-        <Typography className="Portada-Leyenda">Compramos procesadores, placas, y componentes tecnológicos en deshuso</Typography>
+        <Typography className="Portada-Titulo">
+          Reciclaje de componentes electrónicos
+        </Typography>
+        <Typography className="Portada-Leyenda">
+          Compramos procesadores, placas, y componentes tecnológicos en deshuso
+        </Typography>
       </div>
 
       <Link to="/productos" className="Portada-Action">
+        <Button variant="contained" className="Action-Button">
+          VER PRODUCTOS
+        </Button>
+      </Link>*/}
+      <div className="LandingPage-Background">
+        <img src={backgroundLanding} />
+      </div>
 
-        <Button
-          variant="contained"
-          className="Action-Button"
-        >VER PRODUCTOS</Button>
-      </Link>
-      
       <a className="WhatsaApp-Button" href="https://wa.me/5491157508133">
-        <img src={WhatsAppIcon}/>
+        <img src={WhatsAppIcon} />
       </a>
     </div>
-  )
-}
+  );
+};
 
 export default LandingPage;
